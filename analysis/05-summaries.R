6dfd0af8b76ac1dbb701477bae690d394dfa41d5
Peter Solymos <psolymos@gmail.com>
2025-09-23 12:06:11 +0200

    Update
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- analysis/05-summaries.R
+++ analysis/05-summaries.R
@@ -1,5 +1,11 @@
 ## bSims simulations
 
+fl <- c(list.files("_tmp/est_conv", full.names = TRUE),
+    list.files("_tmp/est_n1000", full.names = TRUE))
+# fl <- c(list.files("_tmp/est_conv", full.names = TRUE))
+xb <- do.call(rbind, lapply(fl, readRDS))
+arrow::write_parquet(xb, "estimates_bsims.parquet")
+
 fl <- c(list.files("_tmp/est_conv_mc", full.names = TRUE))
 xb <- do.call(rbind, lapply(fl, readRDS))
 arrow::write_parquet(xb, "estimates_bsims_mc.parquet")
@@ -7,5 +13,8 @@ arrow::write_parquet(xb, "estimates_bsims_mc.parquet")
 ## field data
 
 SPP <- c("CHSP", "DEJU", "HETH", "MAWA", "MYWA", "OVEN", "RCKI", "REVI", "SWTH", "TEWA", "WIWR", "WTSP")
+xf <- do.call(rbind, lapply(paste0("_tmp/paired/estimates_field-data_", SPP, ".rds"), readRDS))
+arrow::write_parquet(xf, "estimates_field_data.parquet")
+
 xf <- do.call(rbind, lapply(paste0("_tmp/paired_mc/estimates_field-data_", SPP, ".rds"), readRDS))
 arrow::write_parquet(xf, "estimates_field_data_mc.parquet")
